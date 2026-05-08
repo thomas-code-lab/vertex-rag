dea558f0c3fa6e34e7b3fa07cd6aaf227994e38d
Thomas P. <thomas.p.work.jp@gmail.com>
2026-05-08 13:41:38 +0900

    Fix langchain prompts import

--- rag_chain.py
+++ rag_chain.py
@@ -13,7 +13,7 @@ import os
 import time
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 
 CHROMA_DIR = "./chroma_db"
